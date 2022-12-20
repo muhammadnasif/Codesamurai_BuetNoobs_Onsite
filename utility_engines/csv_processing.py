f537b52e06010b8366e91bab19018f557e254fa3
Sabbir <red.abiir@gmail.com>
2022-12-20 19:37:13 +0600

    add proposals data read

--- utility_engines/csv_processing.py
+++ utility_engines/csv_processing.py
@@ -95,7 +95,6 @@ def read_projects_file(project_file):
                 data['timespan'] = int(row[6])
                 data['project_id'] = row[7]
                 data['goal'] = row[8]
-                print(row)
                 data['start_date'] = datetime.strptime(row[9], '%Y-%m-%d')
                 data['completion'] = float(row[10])
                 data['actual_cost'] = float(row[11])
@@ -112,6 +111,50 @@ def read_projects_file(project_file):
                 project_list.append(data)
 
     return project_list, locations
+
+
+# create read_proposals_file like read_projects_file
+def read_proposals_file(proposals_file):
+    # fill rest like last function
+    project_list = []
+    locations = set()
+
+    with open(proposals_file, 'r') as file:
+        reader = csv.reader(file)
+        first = True
+
+        codes = set()
+
+        for row in reader:
+            if first:
+                first = False
+                pass
+            else:
+                data = {}
+                data['name'] = row[0]
+                data['location'] = row[1]
+                data['latitude'] = float(row[2])
+                data['longitude'] = float(row[3])
+                data['exec'] = row[4]
+                data['cost'] = float(row[5])
+                data['timespan'] = int(row[6])
+                data['project_id'] = row[7]
+                data['goal'] = row[8]
+                data['proposal_date'] = datetime.strptime(row[9], '%Y-%m-%d')
+
+                if data['project_id'] in codes:
+                    raise Exception('Duplicate project code: ' + data['code'])
+                elif not data['project_id'].startswith('prop'):
+                    raise Exception('Invalid project code: ' + data['code'])
+                else:
+                    codes.add(data['project_id'])
+
+                locations.add(data['location'])
+
+                project_list.append(data)
+
+    return project_list, locations
+
 
 
 def fill_initial_data():
@@ -166,6 +209,38 @@ def fill_initial_data():
                 start_date=project['start_date'],
                 completion=project['completion'],
                 actual_cost=int(project['actual_cost'])
+            )
+
+            p.save()
+
+    if Proposed_Project.objects.count() == 0:
+        projects, locations = read_proposals_file(settings.BASE_DIR / 'Dataset/proposals.csv')
+
+        for location in locations:
+            if not Location.objects.filter(name=location).exists():
+                l = Location.objects.create(name=location)
+                l.save()
+
+        for project in projects:
+            # create project core
+            core = Project_Core.objects.create(
+                name=project['name'],
+                project_code = project['project_id'],
+                location=Location.objects.get(name=project['location']),
+                executing_agency = Agency.objects.get(code=project['exec']),
+                latitude=project['latitude'],
+                longitude=project['longitude'],
+                expected_cost = int(project['cost'] * 10000000),
+                timespan = project['timespan'],
+                goal = project['goal'],
+                is_approved = False
+            )
+
+            core.save()
+
+            p = Proposed_Project.objects.create(
+                project=core,
+                proposed_date=project['proposal_date']
             )
 
             p.save()
