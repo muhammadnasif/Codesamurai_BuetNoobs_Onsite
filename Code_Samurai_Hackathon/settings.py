57214f08f6e98bfc618c881795695ee11748f18c
Sabbir <red.abiir@gmail.com>
2022-12-21 08:11:49 +0600

    add csrf allowed sites

--- Code_Samurai_Hackathon/settings.py
+++ Code_Samurai_Hackathon/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://buetnoobs.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['buet-noobs-hackathon.up.railway.app']
 
 # Application definition
 
